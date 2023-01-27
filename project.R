29c696dcff4505c0ab98e4a35541e61773eb7104
Panyloi <panyloi@git.com>
2023-01-27 04:52:07 +0100

    Final project

--- project.R
+++ project.R
@@ -60,45 +60,45 @@ ackleyMs2 <- replicate(50, msFun(2, -32.768, 32.768, TRUE))
 ackleyMs10 <- replicate(50, msFun(10, -32.768, 32.768, TRUE))
 ackleyMs20 <- replicate(50, msFun(20, -32.768, 32.768, TRUE))
 
-rastringMs2 <- replicate(50, msFun(2, -5.12, 5.12, FALSE))
-rastringMs10 <- replicate(50, msFun(10, -5.12, 5.12, FALSE))
-rastringMs20 <- replicate(50, msFun(20, -5.12, 5.12, FALSE))
+rastriginMs2 <- replicate(50, msFun(2, -5.12, 5.12, FALSE))
+rastriginMs10 <- replicate(50, msFun(10, -5.12, 5.12, FALSE))
+rastriginMs20 <- replicate(50, msFun(20, -5.12, 5.12, FALSE))
 
 ackley2 <- replicate(50, rndFunction1(2, mean(ackleyMs2[1,]), TRUE))
 ackley10 <- replicate(50, rndFunction1(10, mean(ackleyMs10[1,]), TRUE))
 ackley20 <- replicate(50, rndFunction1(20, mean(ackleyMs20[1,]), TRUE))
 
-rastring2 <- replicate(50, rndFunction1(2, mean(rastringMs2[1, ]), FALSE))
-rastring10 <- replicate(50, rndFunction1(10, mean(rastringMs10[1, ]), FALSE))
-rastring20 <- replicate(50, rndFunction1(20, mean(rastringMs20[1, ]), FALSE))
+rastrigin2 <- replicate(50, rndFunction1(2, mean(rastriginMs2[1, ]), FALSE))
+rastrigin10 <- replicate(50, rndFunction1(10, mean(rastriginMs10[1, ]), FALSE))
+rastrigin20 <- replicate(50, rndFunction1(20, mean(rastriginMs20[1, ]), FALSE))
 
 
 
-hist(ackley2, breaks = 50, main = "Funkcja Ackley'a w 2 wymiarach - PRS", xlab = "Wartość minimum", ylab = "Ilość", axes = FALSE)
-axis(1,at=seq(-1000, 1000,by=0.1))
-axis(2,at=seq(0, 50,by=1))
+hist(ackley2, main = "Funkcja Ackley'a w 2 wymiarach - PRS", xlab = "Wartość minimum", ylab = "Ilość")
+# axis(1,at=seq(-1000, 1000,by=0.1))
+# axis(2,at=seq(0, 50,by=1))
 
-hist(ackley10, breaks = 50, main = "Funkcja Ackley'a w 10 wymiarach - PRS", xlab = "Wartość minimum", ylab = "Ilość", axes = FALSE)
-axis(1,at=seq(-1000, 1000,by=0.1))
-axis(2,at=seq(0, 50,by=1))
+hist(ackley10, main = "Funkcja Ackley'a w 10 wymiarach - PRS", xlab = "Wartość minimum", ylab = "Ilość")
+# axis(1,at=seq(-1000, 1000,by=0.1))
+# axis(2,at=seq(0, 50,by=1))
 
-hist(ackley20, breaks = 50, main = "Funkcja Ackley'a w 20 wymiarach - PRS", xlab = "Wartość minimum", ylab = "Ilość", axes = FALSE)
-axis(1,at=seq(-1000, 1000,by=0.1))
-axis(2,at=seq(0, 50,by=1))
+hist(ackley20, main = "Funkcja Ackley'a w 20 wymiarach - PRS", xlab = "Wartość minimum", ylab = "Ilość")
+# axis(1,at=seq(-1000, 1000,by=0.1))
+# axis(2,at=seq(0, 50,by=1))
 
 
 
-hist(ackleyMs2[2,], breaks = 50, main = "Funkcja Ackley'a w 2 wymiarach - MS", xlab = "Wartość minimum", ylab = "Ilość")
-axis(1,at=seq(-10000, 10000,by=100))
-axis(2,at=seq(-50, 50,by=5))
+hist(ackleyMs2[2,], main = "Funkcja Ackley'a w 2 wymiarach - MS", xlab = "Wartość minimum", ylab = "Ilość")
+# axis(1,at=seq(-10000, 10000,by=100))
+# axis(2,at=seq(-50, 50,by=5))
 
-hist(ackleyMs10[2,], breaks = 50, main = "Funkcja Ackley'a w 10 wymiarach - MS", xlab = "Wartość minimum", ylab = "Ilość")
-axis(1,at=seq(-10000, 10000,by=100))
-axis(2,at=seq(0, 50,by=5))
+hist(ackleyMs10[2,], main = "Funkcja Ackley'a w 10 wymiarach - MS", xlab = "Wartość minimum", ylab = "Ilość")
+# axis(1,at=seq(-10000, 10000,by=100))
+# axis(2,at=seq(0, 50,by=5))
 
-hist(ackleyMs20[2,], breaks = 50, main = "Funkcja Ackley'a w 20 wymiarach - MS", xlab = "Wartość minimum", ylab = "Ilość")
-axis(1,at=seq(-10000, 10000,by=100))
-axis(2,at=seq(0, 50,by=5))
+hist(ackleyMs20[2,], main = "Funkcja Ackley'a w 20 wymiarach - MS", xlab = "Wartość minimum", ylab = "Ilość")
+# axis(1,at=seq(-10000, 10000,by=100))
+# axis(2,at=seq(0, 50,by=5))
 
 
 
@@ -113,43 +113,43 @@ boxplot(ackleyMs10[2,], main = "Wykres pudełkowy dla Ackley'a w 10 wymiarach - 
 
 
 
-hist(restring2, breaks = 50, main = "Funkcja Restring'a w 2 wymiarach - PRS", xlab = "Wartość minimum", ylab = "Ilość")
+hist(rastrigin2, main = "Funkcja Rastrigin'a w 2 wymiarach - PRS", xlab = "Wartość minimum", ylab = "Ilość")
 
-hist(restring10, breaks = 50, main = "Funkcja Restring'a w 10 wymiarach - PRS", xlab = "Wartość minimum", ylab = "Ilość", axes = FALSE)
-axis(1,at=seq(-10000, 10000,by=1))
-axis(2,at=seq(0, 50,by=1))
+hist(rastrigin10, main = "Funkcja Rastrigin'a w 10 wymiarach - PRS", xlab = "Wartość minimum", ylab = "Ilość")
+# axis(1,at=seq(-10000, 10000,by=1))
+# axis(2,at=seq(0, 50,by=1))
 
-hist(restring20, breaks = 50, main = "Funkcja Restring'a w 20 wymiarach - PRS", xlab = "Wartość minimum", ylab = "Ilość")
-axis(1,at=seq(-10000, 10000,by=5))
-axis(2,at=seq(0, 50,by=1))
-
-
-
-hist(restringMs2[2,], breaks = 50, main = "Funkcja Restring'a w 2 wymiarach - MS", xlab = "Wartość minimum", ylab = "Ilość", axes = FALSE)
-axis(1,at=seq(-10000, 10000,by=50))
-axis(2,at=seq(0, 50,by=1))
-
-hist(restringMs10[2,], breaks = 50, main = "Funkcja Restring'a w 10 wymiarach - MS", xlab = "Wartość minimum", ylab = "Ilość", axes = FALSE)
-axis(1,at=seq(-10000, 10000,by=50))
-axis(2,at=seq(0, 50,by=1))
-
-hist(restringMs20[2,], breaks = 50, main = "Funkcja Restring'a w 20 wymiarach - MS", xlab = "Wartość minimum", ylab = "Ilość", axes = FALSE)
-axis(1,at=seq(-10000, 10000,by=50))
-axis(2,at=seq(0, 50,by=1))
-
-boxplot(restring2, main = "Wykres pudełkowy dla Restring'a w 2 wymiarach", axes = TRUE)
-boxplot(restringMs2[2,], main = "Wykres pudełkowy dla Restring'a w 2 wymiarach - MS", axes = TRUE)
-boxplot(restringMs10[2,], main = "Wykres pudełkowy dla Restring'a w 10 wymiarach - MS", axes = TRUE)
+hist(rastrigin20, main = "Funkcja Rastrigin'a w 20 wymiarach - PRS", xlab = "Wartość minimum", ylab = "Ilość")
+# axis(1,at=seq(-10000, 10000,by=5))
+# axis(2,at=seq(0, 50,by=1))
 
 
 
-hist(restring2, breaks = 50)
-hist(restring10, breaks = 50)
-hist(restring20, breaks = 50)
-boxplot(restring2)
+hist(rastriginMs2[2,], main = "Funkcja Rastrigin'a w 2 wymiarach - MS", xlab = "Wartość minimum", ylab = "Ilość")
+# axis(1,at=seq(-10000, 10000,by=50))
+# axis(2,at=seq(0, 50,by=1))
+
+hist(rastriginMs10[2,], main = "Funkcja Rastrigin'a w 10 wymiarach - MS", xlab = "Wartość minimum", ylab = "Ilość")
+# axis(1,at=seq(-10000, 10000,by=50))
+# axis(2,at=seq(0, 50,by=1))
+
+hist(rastriginMs20[2,], main = "Funkcja Rastrigin'a w 20 wymiarach - MS", xlab = "Wartość minimum", ylab = "Ilość")
+# axis(1,at=seq(-10000, 10000,by=50))
+# axis(2,at=seq(0, 50,by=1))
+
+boxplot(rastrigin2, main = "Wykres pudełkowy dla Rastrigin'a w 2 wymiarach", axes = TRUE)
+boxplot(rastriginMs2[2,], main = "Wykres pudełkowy dla Rastrigin'a w 2 wymiarach - MS", axes = TRUE)
+boxplot(rastriginMs10[2,], main = "Wykres pudełkowy dla Rastrigin'a w 10 wymiarach - MS", axes = TRUE)
 
 
-hist(restringMs20, breaks = 50)
+
+hist(rastring2)
+hist(rastring10)
+hist(rastring20)
+boxplot(rastring2)
+
+
+hist(rastringMs20)
 
 # MS_resultsAMs2 <- unlist(ackleyMs2[1:length(ackleyMs2)])
 MS_resultsAMs2 <- ackleyMs2[2,]
@@ -163,14 +163,14 @@ PRS_resultsA20 <- ackley20
 
 
 
-MS_resultsRMs2 <- rastringMs2[2,]
-PRS_resultsR2 <- rastring2
+MS_resultsRMs2 <- rastriginMs2[2,]
+PRS_resultsR2 <- rastrigin2
 
-MS_resultsRMs10 <- rastringMs10[2,]
-PRS_resultsR10 <- rastring10
+MS_resultsRMs10 <- rastriginMs10[2,]
+PRS_resultsR10 <- rastrigin10
 
-MS_resultsRMs20 <- rastringMs20[2,]
-PRS_resultsR20 <- rastring20
+MS_resultsRMs20 <- rastriginMs20[2,]
+PRS_resultsR20 <- rastrigin20
 
 mean(MS_resultsAMs2)
 mean(PRS_resultsA2)
@@ -190,7 +190,8 @@ mean(PRS_resultsR10)
 mean(MS_resultsRMs20)
 mean(PRS_resultsR20)
 
-t.test(MS_resultsMs2, PRS_resultsA2)
+# Istotność statystyczna
+t.test(MS_resultsAMs2, PRS_resultsA2)
 t.test(MS_resultsAMs10, PRS_resultsA10)
 t.test(MS_resultsAMs20, PRS_resultsA20)
 
@@ -199,22 +200,10 @@ t.test(MS_resultsRMs10, PRS_resultsR10)
 t.test(MS_resultsRMs20, PRS_resultsR20)
 
 # Analiza wykresów
-# Wykres 1
+
 sd(PRS_resultsA2)
-sd(PRS_resultsA10)
-sd(PRS_resultsA20)
-var(PRS_resultsA20)
+var(PRS_resultsA2)
 summary(PRS_resultsA2)
-summary(PRS_resultsA10)
-summary(PRS_resultsA20)
-
-sd(MS_resultsAMs2)
-sd(MS_resultsAMs10)
-sd(MS_resultsAMs20)
-summary(MS_resultsAMs2)
-summary(MS_resultsAMs10)
-summary(MS_resultsAMs20)
-
 
 
 
